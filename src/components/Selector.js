a5a98105f128057fd255b58ec463527effe0f06b
Jon Fobert <jonrfobert@gmail.com>
2019-07-07 20:11:28 -0400

    prohibit duplicate selection

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -6,19 +6,51 @@ class Selector extends React.Component {
         this.state = {xAxisValue: '', yAxisValue: ''}
     }
 
+    handleXAxisSelected = e => {
+        this.setState({xAxisValue: e.target.value})
+    }
+
+    handleYAxisSelected = e => {
+        this.setState({yAxisValue: e.target.value})
+    }
+
     render() {
+        //Disabled settings for dropdown menus
+        let verticalXDisabled = false;
+        let priceXDisabled = false;
+        let timeXDisabled = false;
+        if(this.state.yAxisValue === 'vertical') {
+            verticalXDisabled = true;
+        } else if (this.state.yAxisValue === 'price') {
+            priceXDisabled = true;
+        } else if (this.state.yAxisValue === 'time') {
+            timeXDisabled = true;
+        }
+
+        let verticalYDisabled = false;
+        let priceYDisabled = false;
+        let timeYDisabled = false;
+        if(this.state.xAxisValue === 'vertical') {
+            verticalYDisabled = true;
+        } else if (this.state.xAxisValue === 'price') {
+            priceYDisabled = true;
+        } else if (this.state.xAxisValue === 'time') {
+            timeYDisabled = true;
+        }
         return(
          <form> Graph: 
-             <select className = "yAxis" name="Y-Axis">
-                 <option value = "vertical" disabled="disabled">Vertical Feet</option>
-                 <option value = "price">Price</option>
-                 <option value = "time">Travel Time</option>
+             <select className = "yAxis" name="Y-Axis" onChange={this.handleYAxisSelected}>
+                 <option value = "yAxis" >---Y Axis</option>
+                 <option value = "vertical" disabled={verticalYDisabled}>Vertical Feet</option>
+                 <option value = "price" disabled ={priceYDisabled}>Price</option>
+                 <option value = "time" disabled ={timeYDisabled}>Travel Time</option>
              </select>
                 versus 
-            <select className = "xAxis" name="X-Axis">
-                <option value = "vertical">Vertical Feet</option>
-                <option value = "price">Price</option>
-                <option value = "time">Travel Time</option>
+            <select className = "xAxis" name="X-Axis" onChange = {this.handleXAxisSelected}>
+                <option value = "xAxis">---X Axis</option>
+                <option value = "vertical" disabled ={verticalXDisabled}>Vertical Feet</option>
+                <option value = "price" disabled ={priceXDisabled}>Price</option>
+                <option value = "time" disabled ={timeXDisabled}>Travel Time</option>
              </select>
          </form>
         )
