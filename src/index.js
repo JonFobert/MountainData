7b2cc782c24ab3a061e2871150eacdab137e2f87
Jon Fobert <jonrfobert@gmail.com>
2019-07-23 21:44:00 -0400

    complete animations

--- src/index.js
+++ src/index.js
@@ -13,28 +13,35 @@ import { CSSTransition } from "react-transition-group";
 class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {xAxisValue: '', yAxisValue: ''}
+        this.state = {xAxisValue: '', yAxisValue: '', selectionChanged: false}
     }
 
     handleXAxisSelected = (e) => {
         this.setState({xAxisValue: e.target.value})
+        this.setState({selectionChanged: true})
     }
 
     handleYAxisSelected = (e) => {
         this.setState({yAxisValue: e.target.value})
+        this.setState({selectionChanged: true})
     }
 
     render() {
         let graph;
+        //if both x axis and Y axis are selected then create the graph
         if(this.state.xAxisValue && this.state.yAxisValue) {
             graph = 
             <CSSTransition
-                in={true}
+                //if the selection for the x or y axis changed recently than animate the graph appearing or enterin
+
+                in={this.state.selectionChanged}
                 appear={true}
                 timeout = {{
                     appear: 800,
                     enter: 300
                 }}
+                //Once the graph has appeared consider the selection unchanged
+                onEntered = {() => this.setState({selectionChanged: false})}
                 classNames="graphFade">
                <Graph xAxisValue = {this.state.xAxisValue}
                       yAxisValue = {this.state.yAxisValue} />
